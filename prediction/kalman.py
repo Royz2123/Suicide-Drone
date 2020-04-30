e14e8c140e42741580ff0f63178df4036a4f213c
איתמר אבליוביץ <T8637718@talpiot.huji.ac.il>
2020-04-30 23:56:38 +0300

    Kalman Justice

--- prediction/kalman.py
+++ prediction/kalman.py
@@ -12,8 +12,17 @@ class KalmanPredictor(object):
     def generate(self, test):
         f = KalmanFilter (dim_x=4, dim_z=4)
         f.H = np.eye(4)
-        f.Q = Q_discrete_white_noise(dim=4, dt=0.1, var=0.13)
-        t, x, y = sorted(test['t']), test['x'], test['y']
+        #f.Q = Q_discrete_white_noise(dim=4, dt=0.1, var=0.0001)
+        f.P *= 0.0001
+        f.R = np.array([[0.0001, 0, 0, 0],
+                        [0, 0.0001, 0, 0],
+                        [0, 0, 0.0001, 0],
+                        [0, 0, 0, 0.0001]])
+        t, x, y = test['t'], test['x'], test['y']
+        x_ind = {p: index for index, p in enumerate(x)}
+        y_ind = {p: index for index, p in enumerate(y)}
+        x, y = sorted(x, key=lambda p: t[x_ind[p]]), sorted(y, key=lambda p: t[y_ind[p]])
+        t = sorted(t)
         t, x, y = np.array(t), np.array(x), np.array(y)
         dts = np.diff(t)
         dx = (x[1:] - x[:-1]) / dts
@@ -29,6 +38,7 @@ class KalmanPredictor(object):
         out_states = []
         for i in range(1, states.shape[1]):
             self.set_dt(f, dts[i-1])
+            f.predict()
             f.update(states[:, i])
 
         self.set_dt(f, out_dt)
