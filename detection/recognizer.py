7a9e7ac33028c350e8417b21f7bae8af389f1fe1
Royz2123 <1Qazwsxdc>
2020-06-01 13:43:00 +0300

    For ron test

--- detection/recognizer.py
+++ detection/recognizer.py
@@ -1,9 +1,11 @@
 import cv2
 import os
+import time
+import numpy as np
 
 
-class DroneRecognizer(object):
-    DEFAUlT_XML_PATH = "./detection/cascades/haarcascade_drones.xml"
+class DroneRecognizer():
+    DEFAUlT_XML_PATH = "./detection/cascades/drone_cascade_beta.xml"
 
     def __init__(self, xml_file=DEFAUlT_XML_PATH, trained=True):
         self._drone_cascade = None
@@ -17,24 +19,42 @@ class DroneRecognizer(object):
     def train(self, im_path="./train_images/drones"):
         pass
 
-    def test(self, im_path="./test_images/drones"):
-        img_paths = [im_path + path for path in os.listdir(im_path)]
+    def test(self, im_path="./test_images/drones/test_images/", viz=False):
+        img_paths = [im_path + path for path in os.listdir(im_path)][:10]
         print("TESTING ON:\t\t", img_paths)
 
-        imgs = [cv2.imread(path) for path in img_paths]
-        imgs = [cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) for img in imgs]
-
-        print(imgs)
+        imgs = [cv2.imread(path, 0) for path in img_paths]
+        times = []
 
         for img in imgs:
-            drones = self._drone_cascade.detectMultiScale(img, 1.3, 5)
+            tick = time.time()
+            drones = self._drone_cascade.detectMultiScale(
+                img,
+                scaleFactor=1.3,
+                minNeighbors=4,
+                # minSize=(10, 10),
+                # maxSize=(500, 500)
+            )
+            tock = time.time()
+
+            times.append(tock - tick)
+            print(f"Detection time:\t\t{times[-1]}")
+
             for (x, y, w, h) in drones:
                 img = cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
-            cv2.imshow('img', img)
-            cv2.waitKey(0)
-            cv2.destroyAllWindows()
 
+            if viz:
+                scale_percent = 30  # percent of original size
+                width = int(img.shape[1] * scale_percent / 100)
+                height = int(img.shape[0] * scale_percent / 100)
+                dim = (width, height)
+                img = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
 
+                cv2.imshow('img', img)
+                cv2.waitKey(0)
+                cv2.destroyAllWindows()
+
+        print(f"Average detection time:\t{np.mean(times)}")
 
 
 
--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import time
 
 import detection.recognizer as recognizer
-import prediction.tester as tester
-
+# import prediction.tester as tester
 
 
 FACE_XML = "./detection/test_cascades/haarcascade_frontalface_alt.xml"
@@ -10,8 +9,8 @@ WATCH_XML = "./train_images/watches/classifier/cascade.xml"
 
 
 def detect():
-    recog_obj = recognizer.DroneRecognizer(xml_file=WATCH_XML)
-    recog_obj.test(im_path="./test_images/watches/")
+    recog_obj = recognizer.DroneRecognizer()
+    recog_obj.test()
 
 
 def predict():
@@ -25,4 +24,4 @@ def predict():
 
 
 if __name__ == "__main__":
-    predict()
+    detect()
