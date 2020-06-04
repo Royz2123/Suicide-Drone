0b0c09ff54295621e943323d6e86f01c5ad52370
T8709339 <ruiyui123@gmail.com>
2020-06-05 00:21:50 +0300

    choosing best drone and considering size

--- detection/recognizer.py
+++ detection/recognizer.py
@@ -6,6 +6,7 @@ import numpy as np
 
 class DroneRecognizer:
     DEFAUlT_XML_PATH = "./detection/cascades/drone_cascade_beta.xml"
+    SIZE_CONFIDENCE = 0.7
 
     def __init__(self, xml_file=DEFAUlT_XML_PATH, trained=True):
         self._drone_cascade = None
@@ -19,30 +20,45 @@ class DroneRecognizer:
     def train(self, im_path="./train_images/drones"):
         pass
 
-    def test(self, im_path="./test_images/drones/test_images/", viz=False):
+    def test(self, im_path="./test_images/drones/test_images/", viz=True):
         img_paths = [im_path + path for path in os.listdir(im_path)][:10]
         print("TESTING ON:\t\t", img_paths)
 
         imgs = [cv2.imread(path, 0) for path in img_paths]
         times = []
-
+        last_size = None
         for img in imgs:
             tick = time.time()
-            drones = self._drone_cascade.detectMultiScale(
-                img,
-                scaleFactor=1.3,
-                minNeighbors=4,
-                # minSize=(10, 10),
-                # maxSize=(500, 500)
-            )
+            if last_size is None:
+                drones = self._drone_cascade.detectMultiScale(
+                    img,
+                    scaleFactor=1.3,
+                    minNeighbors=1,
+                    flags=cv2.CASCADE_DO_CANNY_PRUNING
+                    # minSize=(10, 10),
+                    # maxSize=(500, 500)
+                )
+            else:
+                drones = self._drone_cascade.detectMultiScale(
+                    img,
+                    minNeighbors=1,
+                    flags=cv2.CASCADE_DO_CANNY_PRUNING,
+                    minSize=(last_size[0] * DroneRecognizer.SIZE_CONFIDENCE,
+                             last_size[1] * DroneRecognizer.SIZE_CONFIDENCE),
+                    maxSize=(last_size[0] * (2 - DroneRecognizer.SIZE_CONFIDENCE),
+                             last_size[1] * (2 - DroneRecognizer.SIZE_CONFIDENCE))
+                )
             tock = time.time()
 
             times.append(tock - tick)
             print("Detection time:\t\t", str(times[-1]))
-
-            for (x, y, w, h) in drones:
+            if len(drones)>0:
+                best_drone = max(drones, key=lambda drone: drone[1])
+                ((x, y, w, h), confidence) = best_drone
+                last_size = (w, h)
                 img = cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
-
+            else:
+                last_size=None
             if viz:
                 scale_percent = 30  # percent of original size
                 width = int(img.shape[1] * scale_percent / 100)
@@ -55,6 +71,3 @@ class DroneRecognizer:
                 cv2.destroyAllWindows()
 
         print("Average detection time:\t", str(np.mean(times)))
-
-
-
